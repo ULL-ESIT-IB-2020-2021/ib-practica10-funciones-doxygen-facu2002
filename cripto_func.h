b4294556cdaec6d34946dc5e7a014d5c169a83da
Facundo José García Gallo <alu0101406329@ull.edu.es>
2020-12-13 19:33:14 +0000

    Create cripto_func.h

---
+++ cripto_func.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+std::string EncriptarCesar(std::string frase, int longitud, int k);
+
+std::string DesencriptarCesar(std::string frase, int longitud, int k);
+
+std::string EncriptarXor(std::string frase,int longitud, std::string clave);
